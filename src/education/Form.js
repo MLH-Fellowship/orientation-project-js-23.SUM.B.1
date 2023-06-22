8dffb2ed376ac9472702f27cb7fec31e162911cc
Marcus Chan <marcuscchan@gmail.com>
2023-06-22 12:43:11 -0700

    Added validation

--- src/education/Form.js
+++ src/education/Form.js
@@ -1,4 +1,4 @@
-import { Form as ReactRouterForm } from "react-router-dom"
+import { Form as ReactRouterForm } from "react-router-dom";
 
 export function Form() {
   return (
@@ -6,27 +6,36 @@ export function Form() {
       <div className="form-2-col">
         <label>
           Course:
-          <input type="text" name="course" />
+          <input type="text" name="course" required />
         </label>
         <label>
           School:
-          <input type="text" name="school" />
+          <input type="text" name="school" required />
         </label>
         <label>
           Start Date:
-          <input type="date" name="start_date" />
+          <input type="date" name="start_date" required />
         </label>
         <label>
           End Date:
-          <input type="date" name="end_date" />
+          <input type="date" name="end_date" required />
         </label>
         <label>
           Grade:
-          <input type="text" name="grade" />
+          <input
+            type="text"
+            name="grade"
+            pattern="^(100(\.0{1,2})?|\d{1,2}(\.\d{1,2})?)%$"
+          />
         </label>
         <label>
           Logo (URL):
-          <input type="text" name="logo" />
+          <input
+            type="text"
+            name="logo"
+            pattern="^(http|https):\/\/[^\s/$.?#].[^\s]*$"
+            required
+          />
         </label>
       </div>
       <button>Create</button>
