7ce6206f8295e1638fc9770b9494263b26e4dff1
Marcus Chan <marcuscchan@gmail.com>
2023-06-26 16:08:13 -0700

    Create edit education page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { RootRoute, Route, Router } from '@tanstack/router'
 import { AddEducation } from '@/routes/education/add'
 import { Root } from '@/routes/root'
 import { Index } from '@/routes'
+import { EditEducation } from './routes/education/edit'
 
 const rootRoute = new RootRoute({
   component: Root
@@ -19,7 +20,13 @@ const addEducationRoute = new Route({
   component: AddEducation
 })
 
-const routeTree = rootRoute.addChildren([indexRoute, addEducationRoute])
+const editEducationRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/education/edit/$id',
+  component: EditEducation
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, addEducationRoute, editEducationRoute])
 
 export const router = new Router({ routeTree })
 
